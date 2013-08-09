3078fa34192a8cd0f15ebba50f1d4d5105125de6
Victor Lutin <victor.lutin@gmail.com>
2013-08-09 14:05:50 -0600

    Add a full_title helper

--- Gemfile
+++ Gemfile
@@ -3,15 +3,23 @@ source 'https://rubygems.org'
 ruby '2.0.0'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.0'
+gem 'pg', '0.15.1'
 
 group :development, :test do
-    gem 'sqlite3', '1.3.7'
-    gem 'rspec-rails', '2.13.1'
+#    gem 'sqlite3', '1.3.7'
+  gem 'rspec-rails', '2.13.1'
+  gem 'guard-rspec', '2.5.0'
 end
 
 group :test do
     gem 'selenium-webdriver', '2.0.0'
     gem 'capybara', '2.1.0'
+end
+
+group :development, :test do
+  gem 'spork-rails', github: 'sporkrb/spork-rails'
+  gem 'guard-spork', '1.5.0'
+  gem 'childprocess', '0.3.9'
 end
 
 # Use SCSS for stylesheets
@@ -37,11 +45,11 @@ gem 'jbuilder', '1.0.2'
 
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
-  gem 'sdoc', require: false
+  gem 'sdoc', '0.3.20', require: false
 end
 
 group :production do
-    gem 'pg', '0.15.1'
+#    gem 'pg', '0.15.1'
     gem 'rails_12factor', '0.0.2'
 end
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,6 +8,9 @@ describe "StaticPages" do
 #      response.status.should be(200)
 #    end
 #  end
+
+    let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
     describe "Home Page" do
         it "should have the content 'Sample App'" do
             visit '/static_pages/home'
@@ -16,7 +19,12 @@ describe "StaticPages" do
 
         it "should have the right title" do
             visit '/static_pages/home'
-            expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+            expect(page).not_to have_title("#{base_title} | Home")
+        end
+
+        it "should have the right title" do
+            visit '/static_pages/home'
+            expect(page).to have_title("#{base_title}")
         end
     end
 
@@ -28,7 +36,7 @@ describe "StaticPages" do
         
         it "should have the right title" do
             visit '/static_pages/help'
-            expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+            expect(page).to have_title("#{base_title} | Help")
         end
     end
 
@@ -40,9 +48,20 @@ describe "StaticPages" do
 
       it "should have the right title" do
         visit '/static_pages/about'
-        expect(page).to have_title('Ruby on Rails Tutorial Sample App | About Us')
+        expect(page).to have_title("#{base_title} | About Us")
       end
+    end
 
+    describe "Contact" do
+        it "should have the content 'Contact'" do
+          visit '/static_pages/contact'
+          expect(page).to have_content('Contact')
+        end
+
+        it "should have the right title" do
+            visit '/static_pages/contact'
+            expect(page).to have_title("#{base_title} | Contact")
+        end
     end
 
 end
