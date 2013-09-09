d7dfb759df146350e70f53277f2a3ffa898cd3ab
Victor Lutin <victor.lutin@gmail.com>
2013-09-09 00:30:52 -0600

    excersices chap 9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
+  before_action :not_needed,     only: [:new, :create]
 
   def index
     #@users = User.all
@@ -71,4 +72,7 @@ class UsersController < ApplicationController
         redirect_to(root_url) unless current_user.admin?
     end
 
+    def not_needed
+        redirect_to(root_url) unless !signed_in?
+    end
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -78,14 +78,24 @@ describe "AuthenticationPages" do
       describe "when attempting to visit a protected page" do
         before do
           visit edit_user_path(user)
-          fill_in "Email",            with: user.email
-          fill_in "Password",         with: user.password
-          click_button "Sign in"
+          sign_in user
         end
 
         describe "after signing in" do
           it "should render the desired protected page" do
             expect(page).to have_title('Edit user')
+          end
+
+          describe "when signing in again" do
+            before do
+              delete signout_path
+              visit signin_path
+              sign_in user
+            end
+
+            it "should render the default (profile) page" do
+              expect(page).to have_title(user.name)
+            end
           end
         end
       end
@@ -108,15 +118,15 @@ describe "AuthenticationPages" do
     end
 
     describe "as non-admin user" do
-        let(:user) { FactoryGirl.create(:user) }
-        let(:non_admin) {FactoryGirl.create(:user) }
+      let(:user) { FactoryGirl.create(:user) }
+      let(:non_admin) {FactoryGirl.create(:user) }
 
-        before { sign_in non_admin, no_capybara: true }
+      before { sign_in non_admin, no_capybara: true }
 
-        describe "submitting a DELETE request to the Users#destroy action" do
-            before { delete user_path(user) }
-            specify { expect(response).to redirect_to(root_url) }
-        end
+      describe "submitting a DELETE request to the Users#destroy action" do
+        before { delete user_path(user) }
+        specify { expect(response).to redirect_to(root_url) }
+      end
     end
   end
 end
